da600afe572301b87e47dfe9341045eaaa0d9b3e
mkoertgen <marcel.koertgen@gmail.com>
2015-08-18 17:43:12 +0200

    - removed MSBuild Community Tasks since not (yet) Nuget 3.0 compatible. + added GitVersionTask

--- OneClickBuild/tools/Uninstall.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Import-Module (Join-Path $toolsPath "OneClickBuild.psm1")
-
-function Main {
-	Remove-Import '.build\build.targets' $project.Name
-}
-
-Main
